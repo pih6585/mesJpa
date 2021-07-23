668700012cba93c0b69aec15fa7057c623a7e82d
PIH6585 <pih6585@naver.com>
2021-07-23 17:26:24 +0900

    feat : 생성, 수정, 재고증가, 재고감소 기능 추가, 제품의 재고정보 포함여부 추가 test : 생성, 수정, 재고증가, 재고감소, 제품의 재고정보 포함 테스트 및 예외처리

--- src/main/java/com/mes/jpa/entity/ItemStock.java
+++ src/main/java/com/mes/jpa/entity/ItemStock.java
@@ -17,16 +17,47 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ItemStock extends BaseEntity {
 
+	private final String ITEM_STOCK_ID = "stock_id";
+	private final String ITEM_ID = "item_id";
 	@Id
 	@GeneratedValue
-	@Column(name = "stock_id")
+	@Column(name = ITEM_STOCK_ID, updatable = false)
 	private Long id;
 
 	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "item_id")
+	@JoinColumn(name = ITEM_ID)
 	private Item item;
 
+	@Column(unique = true)
 	private String serialNumber;
 
 	private int stockQty;
+
+	private ItemStock(long id, Item item, String serialNumber, int stockQty) {
+		this.id = id;
+		this.item = item;
+		this.serialNumber = serialNumber;
+		this.stockQty = stockQty;
+		item.getItemStocks().add(this);
+	}
+
+	public static ItemStock createStock(long id, Item item, String serialNumber, int stockQty) {
+		return new ItemStock(id,item,serialNumber,stockQty);
+	}
+
+	public static ItemStock updateStock(Long id, Item item, String serialNumber, int stockQty) {
+		return new ItemStock(id,item,serialNumber,stockQty);
+	}
+
+	public int addStock(int stockQty) {
+		return this.stockQty += stockQty;
+	}
+
+	public int minusStock(int stockQty) {
+		int calculateQty = this.stockQty - stockQty;
+		if(calculateQty < 0){
+			throw new IllegalArgumentException("재고수량이 0 이하가 될수 없습니다.");
+		}
+		return calculateQty;
+	}
 }
---
+++ src/test/java/com/mes/jpa/entity/ItemStockTest.java
@@ -0,0 +1,82 @@
+package com.mes.jpa.entity;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ItemStockTest {
+
+	@Test
+	@DisplayName("제품정보 및 수량 및 시리얼 번호를 입력하면 재고가 저장된다.")
+	public void createStock() {
+		Item item = Item.createItem(1L, "1자드라이버", ItemType.자재, "1x2", "테스트자재");
+
+		ItemStock result = getItemStock(1L, item, "1234", 100);
+		assertThat(result.getItem()).isEqualTo(item);
+		assertThat(result.getId()).isEqualTo(1L);
+	}
+
+	@Test
+	@DisplayName("재고정보를 수정하면 재고가 수정된다.")
+	public void updateStock() {
+		Item item = Item.createItem(1L, "1자드라이버", ItemType.자재, "1x2", "테스트자재");
+
+		ItemStock result = getItemStock(1L, item, "1234", 100);
+		ItemStock updateItemStock = ItemStock.updateStock(2L,result.getItem(),result.getSerialNumber(),150);
+
+		assertThat(updateItemStock.getStockQty()).isEqualTo(150);
+		assertThat(updateItemStock).isEqualTo(result);
+	}
+
+	@Test
+	@DisplayName("재고수량을 추가하면 재고가 증가된다.")
+	public void addStock() {
+		Item item = Item.createItem(1L, "1자드라이버", ItemType.자재, "1x2", "테스트자재");
+		ItemStock result = getItemStock(1L, item, "1234", 100);
+
+		int addStockValue = result.addStock(200);
+
+		assertThat(addStockValue).isEqualTo(300);
+	}
+
+	@Test
+	@DisplayName("재고수량을 삭제하면 재고가 감소된다.")
+	public void minusStock() {
+		Item item = Item.createItem(1L, "1자드라이버", ItemType.자재, "1x2", "테스트자재");
+		ItemStock result = getItemStock(1L, item, "1234", 200);
+
+		int minusStockValue = result.minusStock(100);
+
+		assertThat(minusStockValue).isEqualTo(100);
+	}
+
+	@Test
+	@DisplayName("재고수량을 감소시 0보다 작으면  예외가 발생된다.")
+	public void minusStock_zero_check() {
+		Item item = Item.createItem(2L, "1자드라이버", ItemType.자재, "1x2", "테스트자재");
+		ItemStock result = getItemStock(2L, item, "4586", 100);
+
+		assertThatThrownBy(() -> result.minusStock(200))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessage("재고수량이 0 이하가 될수 없습니다.");
+	}
+
+	@Test
+	@DisplayName("재고에 저장된 제품정보와 제품에 저장된 재고 정보가 포함되어 있다.")
+	public void itemsStockByItemContainsItemByItemStock() {
+		Item item = Item.createItem(2L, "1자드라이버", ItemType.자재, "1x2", "테스트자재");
+		getItemStock(2L, item, "4586", 100);
+		getItemStock(1L, item, "4885", 50);
+
+		List<ItemStock> itemStocks = item.getItemStocks();
+		assertThat(itemStocks).extracting("serialNumber").containsExactly("4586", "4885");
+		assertThat(itemStocks).extracting("stockQty").containsExactly(100,50);
+	}
+
+	private ItemStock getItemStock(Long id, Item item, String serialNumber, int stockQty) {
+		return ItemStock.createStock(id, item, serialNumber, stockQty);
+	}
+}
