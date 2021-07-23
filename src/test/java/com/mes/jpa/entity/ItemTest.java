03567f7abba3857716db2cc87e38afe71ff69082
PIH6585 <pih6585@naver.com>
2021-07-23 17:09:42 +0900

    fix : static instance로 변경에 따른 수정

--- src/test/java/com/mes/jpa/entity/ItemTest.java
+++ src/test/java/com/mes/jpa/entity/ItemTest.java
@@ -1,19 +1,10 @@
 package com.mes.jpa.entity;
 
 import static org.assertj.core.api.Assertions.*;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class ItemTest {
-
-	private Item item;
-
-	@BeforeEach
-	void setup(){
-		item = new Item();
-	}
-
 
 	@Test
 	@DisplayName("아이템 정보를 입력하면 아이템정보가 생성된다.")
@@ -29,7 +20,7 @@ class ItemTest {
 	public void updateItem() {
 		Item createItem = getItem(2L,"나사",ItemType.제품,"2x2","서브자재");
 
-		Item updateItem = createItem.updateItem(createItem.getItemName(), createItem.getItemType(),"2x3","재생산");
+		Item updateItem = Item.updateItem(createItem.getId(),createItem.getItemName(), createItem.getItemType(),"2x3","재생산");
 
 		assertThat(updateItem.getItemName()).isEqualTo(createItem.getItemName());
 		assertThat(updateItem.getItemSpec()).isEqualTo("2x3");
@@ -37,7 +28,7 @@ class ItemTest {
 	}
 
 	private Item getItem(long id, String itemName, ItemType itemType, String itemSpec, String remark) {
-		return item.createItem(id, itemName, itemType, itemSpec, remark);
+		return Item.createItem(id, itemName, itemType, itemSpec, remark);
 	}
 
 }
